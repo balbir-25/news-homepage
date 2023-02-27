04fc4793b43f2677f2dae822eab6968873ff788b
Balbir Singh <83856212+balbir-25@users.noreply.github.com>
2023-02-27 22:10:08 +0530

    site

---
+++ index.js
@@ -0,0 +1,19 @@
+const openBtn = document.querySelector('.open');
+const closeBtn = document.querySelector('.close');
+const navUl = document.querySelector('.nav-ul');
+
+openBtn.addEventListener('click', () => {
+  openBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+  navUl.style.display = 'block';
+  navUl.style.animation = 'slide-in 0.5s forwards';
+});
+
+closeBtn.addEventListener('click', () => {
+  closeBtn.style.display = 'none';
+  navUl.style.animation = 'slide-out 0.5s forwards';
+  setTimeout(() => {
+    navUl.style.display = 'none';
+    openBtn.style.display = 'block';
+  }, 500);
+});
